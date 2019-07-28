39618e55c9749b6a01e9692247a9f494829645bc
stefan <stefan@stefans-iMac.local>
2019-07-28 20:54:22 +0200

    Now getting job titles

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
 
 const scrapingResults = [
   {
@@ -16,7 +17,12 @@ const scrapingResults = [
 async function main() {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto("https://www.google.com");
+  await page.goto(
+    "https://sfbay.craigslist.org/d/software-qa-dba-etc/search/sof"
+  );
+  const html = await page.content();
+  const $ = cheerio.load(html);
+  $(".result-title").each((index, element) => console.log($(element).text()));
 }
 
 main();
